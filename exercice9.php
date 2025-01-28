fe9625ac24da0efcd70f9ad21eb3183ac052559b
Marie-France Fritz <mariefrance.fritz@outlook.fr>
2025-01-28 16:56:23 +0100

    add exercice 9

--- exercice9.php
+++ exercice9.php
@@ -8,4 +8,21 @@
  * si l’âge du spectateur ($viewerAge) est supérieur ou égal à ageRestriction, 
  * sinon false.
   * En dessous de la classe, créer un objet et appeler les méthodes
- */
+*/
+
+class Movie{
+
+  public function __construct(protected string $title, protected string $genre, protected int $ageRestriction){}
+
+  public function canWatch($viewerAge){
+    if ($viewerAge >= $this->ageRestriction){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
+
+$movie1 = new Movie("titre 1", "Romance", 12);
+$movie1->canWatch(10);
