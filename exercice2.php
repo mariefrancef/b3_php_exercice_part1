7d916a168d860f4d1f6c1fb788172f8e0a8ba4f8
Marie-France Fritz <mariefrance.fritz@outlook.fr>
2025-01-28 16:24:18 +0100

    fix exercice 2

--- exercice2.php
+++ exercice2.php
@@ -11,27 +11,69 @@
 
  class Person{
 
-  public function __construct(protected string $first_name, protected string $last_name, protected int $age){}
+  public function __construct(
+    protected string $first_name, 
+    protected string $last_name, 
+    protected int $age){}
+
+
+
+  public function afficheInfos(){
+    echo $this->first_name." ".$this->last_name.", ".$this->age." years"."</br>";
+  }
 
   /**
    * Get the value of first_name
-  */
+   */
   public function getFirstName(): string
   {
-    return $this->first_name;
+      return $this->first_name;
   }
 
   /**
    * Set the value of first_name
-  */
+   */
   public function setFirstName(string $first_name): self
   {
-    $this->first_name = $first_name;
-    return $this;
+      $this->first_name = $first_name;
+
+      return $this;
   }
 
-  public function afficheInfos(){
-    echo $this->first_name." ".$this->last_name.", ".$this->age." years"."</br>";
+  /**
+   * Get the value of last_name
+   */
+  public function getLastName(): string
+  {
+      return $this->last_name;
+  }
+
+  /**
+   * Set the value of last_name
+   */
+  public function setLastName(string $last_name): self
+  {
+      $this->last_name = $last_name;
+
+      return $this;
+  }
+
+  /**
+   * Get the value of age
+   */
+  public function getAge(): int
+  {
+      return $this->age;
+  }
+
+  /**
+   * Set the value of age
+   */
+  public function setAge(int $age): self
+  {
+      $this->age = $age;
+
+      return $this;
   }
 }
 
