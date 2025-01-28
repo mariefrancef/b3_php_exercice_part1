dd822bdbce1ab81a78759728bb0e7b2681a8f064
Marie-France Fritz <mariefrance.fritz@outlook.fr>
2025-01-28 16:34:29 +0100

    add exercice 6

--- exercice6.php
+++ exercice6.php
@@ -10,4 +10,42 @@
  * Implémentez une méthode pour afficher tous les items.
  * Implémentez une méthode pour calculer le coût total.
  * En dessous de la classe, créer un objet et appeler vos méthodes
- */
+*/
+
+class ShoppingCart
+{
+  public $items = [];
+  public $total = 0;
+
+
+  public function addItem($name, $price)
+  {
+    $this->items[] = ['name' => $name, 'price' => $price];
+  }
+
+  public function displayItems()
+  {
+    foreach ($this->items as $item) {
+      echo "Name : {$item['name']}, price : {$item['price']}€"."</br>";
+    }
+  }
+
+  public function calculateTotal()
+  {
+    $this->total = 0;
+    foreach ($this->items as $item) {
+      $this->total = $this->total + $item['price'];
+    }
+    return $this->total;
+  }
+}
+
+$cart1 = new ShoppingCart();
+
+$cart1->addItem("Book", 15);
+$cart1->addItem("Pensil", 2);
+$cart1->addItem("Paper", 5);
+
+
+echo "Articles in the cart : ".$cart1->displayItems();
+echo "Total price : " . $cart1->calculateTotal() . "€"."</br>";
