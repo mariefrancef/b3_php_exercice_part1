16455718f4640c6e5927899d6825657c8d3ee521
Marie-France Fritz <mariefrance.fritz@outlook.fr>
2025-01-28 16:42:37 +0100

    add exercice 7

--- exercice7.php
+++ exercice7.php
@@ -8,3 +8,27 @@
  * Affichez un message lors de chaque opération.
  * En dessous de la classe, créer un objet et appeler les méthodes
 */
+
+class BankAccount{
+
+  public function __construct(protected float $balance){}
+
+  public function deposit(float $amount){
+    $newBalanceAfterDeposit = $this->balance + $amount;
+    echo "Your balance is : ".$newBalanceAfterDeposit."</br>";
+  }
+
+  public function withdraw(float $amount){
+    if($this->balance >= $amount){
+      $newBalanceAfterWithdraw = $this->balance - $amount;
+      echo "Your balance is : ".$newBalanceAfterWithdraw."</br>";
+    }
+    else{
+      echo "Amount is too big for withdraw";
+    }
+  }
+}
+
+$account1 = new BankAccount(250);
+$account1->deposit(50);
+$account1->withdraw(100);
